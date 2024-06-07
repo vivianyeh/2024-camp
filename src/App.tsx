b6f29c69fdb9d30ba8cac4006274925137ec4d22
Vivian Yeh <tetsio1123@gmail.com>
2024-06-07 23:03:41 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import Protfolio from "./pages/Portfolio";
+import Protfolio from "./pages/Portfolio.tsx";
 
 function App() {
   return (<>
